91193789aae214ad167ea3a5cba3fc3c773539e3
akbaridc <akbarimawan18@gmail.com>
2021-11-28 13:28:58 +0700

    Slicing Checkout, CHeckout Success and templating

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,11 @@ Route::get('/', function () {
 Route::get('/login', function () {
     return view('login');
 })->name('login');
+
+Route::get('/checkout', function () {
+    return view('checkout');
+})->name('checkout');
+
+Route::get('/success-checkout', function () {
+    return view('success_checkout');
+})->name('success-checkout');
