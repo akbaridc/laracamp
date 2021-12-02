2f78b23c23e0cbe6abcd848560e2e471a2b06f3c
akbaridc <akbarimawan18@gmail.com>
2021-12-03 00:25:19 +0700

    set to paid

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Models\Checkouts;
+use App\Mail\Checkout\Paid;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
 
 class DashboardController extends Controller
 {
@@ -19,6 +21,9 @@ class DashboardController extends Controller
     {
         $checkouts->is_paid = true;
         $checkouts->save();
+
+        Mail::to($checkouts->User->email)->send(new Paid($checkouts));
+
         $request->session()->flash('success', "Checkout with ID {$checkouts->id} has been updated");
         return redirect(route('admin.dashboard'));
     }
